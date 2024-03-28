fa93e1d0e667690f654dc608f56ddf53bb395322
mavr1ke <mainstip@mail.ru>
2024-03-28 11:24:42 +0100

    update

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import Registration from "../components/Register/Registration";
+import RegistrationPage from "../pages/Registration/RegistrationPage";
 import CourseDetailsP from "../pages/CouresesDetails/CouresesDetailsP";
 
 const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/reg" element={<Registration/>} />
+      <Route path="/reg" element={<RegistrationPage/>} />
       <Route path="/course_detail" element={<CourseDetailsP/>} />
     </Routes>
   );
