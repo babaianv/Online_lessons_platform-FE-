96b442fa7ebac28e1d89691facf159f752170c2b
mavr1ke <mainstip@mail.ru>
2024-04-06 20:32:16 +0200

    update

--- src/pages/MyCoursesAndCreatedCourses/MyCoursesPage.tsx
+++ src/pages/MyCoursesAndCreatedCourses/MyCoursesPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-// import Header from '../../components/Header/Header'
-// import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 import CoursePanel from '../../components/CoursePanel/CoursePanel'
 
 const MyCoursesPage: React.FC = () => {
   return (
     <>
-    {/* <Header /> */}
+    <Header />
     <CoursePanel />
-    {/* <Footer /> */}
+    <Footer />
     </>
   )
 }
