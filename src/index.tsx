201eb15397b4cf317b96d463298bd1362da8963e
mavr1ke <mainstip@mail.ru>
2024-04-09 09:58:01 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
+    <ToastContainer position="bottom-left" autoClose={5000} />
   </Provider>,
 )
