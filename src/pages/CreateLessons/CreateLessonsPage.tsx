28a16f6cc8c95b4e31cf9a5461147c6b936a9929
mavr1ke <mainstip@mail.ru>
2024-04-12 23:49:24 +0200

    update

--- src/pages/CreateLessons/CreateLessonsPage.tsx
+++ src/pages/CreateLessons/CreateLessonsPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import CreateLessons from '../../components/CreateLessons/CreateLessons'
-// import Header from '../../components/Header/Header'
-// import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
 
 const CreateLessonsPage: React.FC = () => {
   return (
     <>
-    {/* <Header /> */}
+    <Header />
     <CreateLessons />
-    {/* <Footer /> */}
+    <Footer />
   </>
   )
 }
